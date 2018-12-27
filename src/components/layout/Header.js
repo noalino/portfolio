84715485d0aa72809167fd818f3e2e90429246da
Noalino <bgelineau.dev@gmail.com>
2018-12-27 20:08:17 +0100

    Create navbar toggle & add social media icons

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
 
 import styles from '../../styles/layout/header.module.scss';
 // Optimize SVG (SVG Optimizer) and use inline SVG? (color control)
 import logo from '../../images/logo.svg';
 
-const Header = () => (
-  <header>
+const Header = ({ showNav, toggleNavbar }) => (
+  <header
+    nav={showNav ? 'show' : 'hide'}
+  >
     <Link to="/">
       <img
         src={logo}
@@ -17,10 +20,16 @@ const Header = () => (
     <button
       type="button"
       className={styles.menu_btn}
+      onClick={toggleNavbar}
     >
       <div className={styles.menu_btn_icon} />
     </button>
   </header>
 );
+
+Header.propTypes = {
+  showNav: PropTypes.bool.isRequired,
+  toggleNavbar: PropTypes.func.isRequired,
+};
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-// import { Link } from 'gatsby';
+import { Link } from 'gatsby';
 
+import logoGithub from '../images/github.svg';
+import logoLinkedin from '../images/linkedin.svg';
+import logoTwitter from '../images/twitter.svg';
+
+import styles from '../styles/index.module.scss';
 // import Image from '../components/image';
 // import SEO from '../components/seo';
 // import Home from '../components/pages/Home';
@@ -12,15 +17,20 @@ const Index = () => (
     <h1>BENOIT GELINEAU</h1>
     <h2>FULL STACK JAVASCRIPT DEVELOPER</h2>
     <h3>REACT | NODE | EXPRESS | MONGODB</h3>
-    <button
-      type="button"
-    >
+    <Link to="/contact">
       CONTACT ME
-    </button>
-    <div>
-      <img alt="GitHub" />
-      <img alt="LinkedIn" />
-      <img alt="Twitter" />
+    </Link>
+    <div className={styles.icons}>
+      {/* Set personal links */}
+      <a href="https://github.com">
+        <img src={logoGithub} alt="GitHub" />
+      </a>
+      <a href="https://linkedin.com">
+        <img src={logoLinkedin} alt="LinkedIn" />
+      </a>
+      <a href="https://twitter.com">
+        <img src={logoTwitter} alt="Twitter" />
+      </a>
     </div>
   </>
 );
