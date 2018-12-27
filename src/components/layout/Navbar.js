4b63af58586ec81c38dc717caf92341690d5ad5c
Benoît Gelineau <bgelineau.dev@gmail.com>
2018-12-27 19:04:21 +0100

    Add navbar style

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 
+import styles from '../../styles/layout/navbar.module.scss';
+
 const Navbar = ({ menuLinks }) => (
-  <nav>
-    <ul>
+  <nav id={styles.nav}>
+    <button
+      type="button"
+      aria-label="Close Navigation Bar"
+    >
+      <span>&times;</span>
+    </button>
+    <ul className={styles.nav_links}>
       {menuLinks.map(item => (
         <li key={item.name}>
           <Link to={item.link}>
