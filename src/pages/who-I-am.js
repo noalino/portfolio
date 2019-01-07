bd72445578720b4eed703376aebe056a5b1fe538
Benoît Gelineau <bgelineau.dev@gmail.com>
2019-01-07 19:02:58 +0100

    Style Who-i-am page

--- src/pages/who-I-am.js
+++ src/pages/who-I-am.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-import SEO from '../components/seo';
+// import SEO from '../components/seo';
+import styles from '../styles/pages/who-i-am.module.scss';
 
 const About = () => (
-  <>
+  <div className={styles.content}>
     {/* <SEO title="Page two" /> */}
     <h1>Who I Am.</h1>
     <div>
-      <img alt="Benoît Gelineau" />
+      <img alt="Benoît Gelineau's head" />
     </div>
     <p>I am a self-taught web developer based in Cholet, France and I enjoy building web applications from scratch when I am not playing drums.</p>
-  </>
+  </div>
 );
 
 export default About;
