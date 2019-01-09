5dc50f64026e449930e141b27ea28f4eb5cf1dd3
Noalino <bgelineau.dev@gmail.com>
2019-01-09 11:35:43 +0100

    Create style contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,59 +1,63 @@
 import React from 'react';
 
-import Footer from '../components/layout/Footer';
+// import Footer from '../components/layout/Footer';
+
+import styles from '../styles/pages/contact.module.scss';
 
 const Contact = () => (
-  <>
+  <div className={styles.container}>
     <h1>How to contact me.</h1>
     <form
+      id={styles.contact_form}
       action="https://formspree.io/bgelineau.dev@gmail.com"
       method="POST"
     >
       <label htmlFor="name">
         Name:
-        <input
-          id="name"
-          name="name"
-          type="text"
-          required
-        />
       </label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        autoComplete="off"
+        required
+      />
       <label htmlFor="email">
         Email:
-        <input
-          id="email"
-          name="email"
-          type="email"
-          required
-        />
       </label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        autoComplete="off"
+        required
+      />
       <label htmlFor="subject">
         Subject:
-        <input
-          id="subject"
-          name="subject"
-          type="text"
-          required
-        />
       </label>
+      <input
+        id="subject"
+        name="subject"
+        type="text"
+        autoComplete="off"
+        required
+      />
       <label htmlFor="message">
         Message:
-        <textarea
-          id="message"
-          name="message"
-          rows="4"
-          cols="50"
-          required
-        />
       </label>
+      <textarea
+        id="message"
+        name="message"
+        required
+      />
       <button
         type="submit"
       >
         Send
       </button>
     </form>
-    <Footer />
-  </>
+    {/* <Footer /> */}
+  </div>
 );
 
 export default Contact;
