c240b6bcb1d0e06eb21b59d8808a3b418ed74fe5
Benoît Gelineau <bgelineau.dev@gmail.com>
2019-01-09 18:22:58 +0100

    Move Footer into Layout component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,65 +1,60 @@
 import React from 'react';
 
-import Footer from '../components/layout/Footer';
-
 import styles from '../styles/pages/contact.module.scss';
 
 const Contact = () => (
-  <>
-    <div className={styles.container}>
-      <h1>How to contact me.</h1>
-      <form
-        id={styles.contact_form}
-        action="https://formspree.io/bgelineau.dev@gmail.com"
-        method="POST"
+  <div className={styles.container}>
+    <h1>How to contact me.</h1>
+    <form
+      id={styles.contact_form}
+      action="https://formspree.io/bgelineau.dev@gmail.com"
+      method="POST"
+    >
+      <label htmlFor="name">
+        Name:
+      </label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        autoComplete="off"
+        required
+      />
+      <label htmlFor="email">
+        Email:
+      </label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        autoComplete="off"
+        required
+      />
+      <label htmlFor="subject">
+        Subject:
+      </label>
+      <input
+        id="subject"
+        name="subject"
+        type="text"
+        autoComplete="off"
+        required
+      />
+      <label htmlFor="message">
+        Message:
+      </label>
+      <textarea
+        id="message"
+        name="message"
+        required
+      />
+      <button
+        type="submit"
       >
-        <label htmlFor="name">
-          Name:
-        </label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          autoComplete="off"
-          required
-        />
-        <label htmlFor="email">
-          Email:
-        </label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          autoComplete="off"
-          required
-        />
-        <label htmlFor="subject">
-          Subject:
-        </label>
-        <input
-          id="subject"
-          name="subject"
-          type="text"
-          autoComplete="off"
-          required
-        />
-        <label htmlFor="message">
-          Message:
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          required
-        />
-        <button
-          type="submit"
-        >
-          Send
-        </button>
-      </form>
-    </div>
-    <Footer />
-  </>
+        Send
+      </button>
+    </form>
+  </div>
 );
 
 export default Contact;
