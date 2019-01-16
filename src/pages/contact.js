471a35bc2d57c023c3365eafb0c6ebc6fd615f2a
Noalino <bgelineau.dev@gmail.com>
2019-01-16 18:29:38 +0100

    Disable focusable areas when navbar is open

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,59 +1,75 @@
 import React from 'react';
 
+import NavbarContext from '../utils/navbar-context';
+
 import styles from '../styles/pages/contact.module.scss';
 
 const Contact = () => (
   <div className={styles.container}>
     <h1>How to contact me.</h1>
-    <form
-      id={styles.contact_form}
-      action="https://formspree.io/bgelineau.dev@gmail.com"
-      method="POST"
-    >
-      <label htmlFor="name">
-        Name:
-      </label>
-      <input
-        id="name"
-        name="name"
-        type="text"
-        autoComplete="off"
-        required
-      />
-      <label htmlFor="email">
-        Email:
-      </label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        autoComplete="off"
-        required
-      />
-      <label htmlFor="subject">
-        Subject:
-      </label>
-      <input
-        id="subject"
-        name="subject"
-        type="text"
-        autoComplete="off"
-        required
-      />
-      <label htmlFor="message">
-        Message:
-      </label>
-      <textarea
-        id="message"
-        name="message"
-        required
-      />
-      <button
-        type="submit"
-      >
-        Send
-      </button>
-    </form>
+    <NavbarContext.Consumer>
+      {showNav => (
+        <form
+          id={styles.contact_form}
+          action="https://formspree.io/bgelineau.dev@gmail.com"
+          method="POST"
+        >
+          <label htmlFor="name">
+            Name:
+          </label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            autoComplete="off"
+            required
+            tabIndex={showNav ? -1 : null}
+            aria-disabled={showNav}
+          />
+          <label htmlFor="email">
+            Email:
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="off"
+            required
+            tabIndex={showNav ? -1 : null}
+            aria-disabled={showNav}
+          />
+          <label htmlFor="subject">
+            Subject:
+          </label>
+          <input
+            id="subject"
+            name="subject"
+            type="text"
+            autoComplete="off"
+            required
+            tabIndex={showNav ? -1 : null}
+            aria-disabled={showNav}
+          />
+          <label htmlFor="message">
+            Message:
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            required
+            tabIndex={showNav ? -1 : null}
+            aria-disabled={showNav}
+          />
+          <button
+            type="submit"
+            tabIndex={showNav ? -1 : null}
+            aria-disabled={showNav}
+          >
+            Send
+          </button>
+        </form>
+      )}
+    </NavbarContext.Consumer>
   </div>
 );
 
