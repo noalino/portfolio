f682d645c964f31463d9d5dfad44d52658c6151b
Noalino <bgelineau.dev@gmail.com>
2019-01-18 19:16:19 +0100

    Add bold words on projects descriptions

---
+++ src/components/projects/descriptions/index.js
@@ -0,0 +1,11 @@
+import NewsAggregator from './NewsAggregator';
+import TicTacToe from './TicTacToe';
+import ChoroplethMap from './ChoroplethMap';
+import SimonGame from './SimonGame';
+
+export default {
+  'News Aggregator': NewsAggregator,
+  'Tic-Tac-Toe Game': TicTacToe,
+  'Choropleth Map': ChoroplethMap,
+  'Simon Game': SimonGame,
+};
