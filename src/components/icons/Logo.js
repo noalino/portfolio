6a040716fdcd0792f9bb28e0cb5bbcebf2a47f54
Noalino <bgelineau.dev@gmail.com>
2019-01-21 19:55:12 +0100

    Style skills icons

--- src/components/icons/Logo.js
+++ src/components/icons/Logo.js
@@ -5,11 +5,12 @@ const Logo = ({ className }) => (
   <svg viewBox="0 0 104 80" className={className}>
     <g fill="none">
       <g transform="translate(2.000000, 3.000000)" strokeWidth="2.5">
-          <g>
-              <polyline points="0 44.800885 25.9136213 12.2787611 39.2336203 28.9956319"/>
-              <polyline points="25.9136213 44.800885 62.7906977 0 99.6677741 44.800885"/>
-          </g>
+          {/* Mountains */}
+          <polyline points="0 44.800885 25.9136213 12.2787611 39.2336203 28.9956319"/>
+          <polyline points="25.9136213 44.800885 62.7906977 0 99.6677741 44.800885"/>
+          {/* Middle bar */}
           <path d="M0,44.800885 L99.6677741,44.800885" strokeLinecap="round"/>
+          {/* Downside */}
           <g transform="translate(0.000000, 44.800885)">
               <path d="M0,0.00971251911 L18.3701008,16.5245144 M66.1129568,30.199115 L99.6938844,0.00971251911"/>
               <path d="M18.2724252,16.4269912 L46.013289,16.4269912" strokeLinecap="round"/>
