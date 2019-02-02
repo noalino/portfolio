b683daeefc4fcf9b1ad96b198bd0e2aff5a09499
Noalino <bgelineau.dev@gmail.com>
2019-02-02 16:46:18 +0100

    Update HTML custom attributes

--- src/components/projects/descriptions/SimonGame.js
+++ src/components/projects/descriptions/SimonGame.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 const SimonGame = () => (
   <>
-    <p>
-      A good old Simon Game written in <strong>plain-vanilla JavaScript</strong>.
-    </p>
-    <p>
-      I wanted the game to look as close to reality as possible and it is remarkable what a few lines of <strong>CSS</strong> can do!
-    </p>
+    <p>A good old Simon Game written in <strong>plain-vanilla JavaScript</strong>.</p>
+    <p>I wanted the game to look as close to reality as possible and it is remarkable what a few lines of <strong>CSS</strong> can do!</p>
   </>
 );
 
