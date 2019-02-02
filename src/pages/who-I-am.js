77080d13864e75a016470c8ed4c14e72fc7dbf6b
Noalino <bgelineau.dev@gmail.com>
2019-02-02 16:09:45 +0100

    Update profile picture

--- src/pages/who-I-am.js
+++ src/pages/who-I-am.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
 // import SEO from '../components/seo';
-// import ProfilePicture from '../components/ProfilePicture';
-// import profile from '../images/profile150x143.png';
-import profile from '../images/me.jpg';
+import ProfilePicture from '../components/ProfilePicture';
+
 import styles from '../styles/pages/who-i-am.module.scss';
 
 const About = () => (
   <div className={styles.container}>
     {/* <SEO title="Page two" /> */}
     <h1>Who I am.</h1>
-    {/* <ProfilePicture /> */}
-    <img src={profile} alt="Benoît Gelineau's profile" style={{ 'width': 150 }} />
+    <ProfilePicture className={styles.picture} />
     <p>I am a self-taught web developer based in Cholet, France and I enjoy building web applications from scratch when I am not playing drums.</p>
   </div>
 );
