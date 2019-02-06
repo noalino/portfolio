555e53e1df4f7af16170d708447a0f433590ca88
Noalino <bgelineau.dev@gmail.com>
2019-02-06 12:11:20 +0100

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import SEO from '../components/SEO';
+import { NavbarContext } from '../utils/context';
 
 import styles from '../styles/pages/404.module.scss';
 
 const NotFoundPage = () => (
-  <div className={styles.container}>
-    <SEO title="404: Not found" />
-    {/* <h1>PAGE NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p> */}
-    <h1>Are you lost?</h1>
-    <p>Go back <Link to="/">Home</Link></p>
-  </div>
+  <NavbarContext.Consumer>
+    {({ showNav }) => (
+      <div className={styles.container}>
+        <SEO title="404: Not found" />
+        <h1>Are you lost?</h1>
+        <p>
+          {`Go back `}
+          <Link
+            to="/"
+            tabIndex={showNav ? -1 : null}
+            aria-disabled={showNav}
+          >
+            Home
+          </Link>
+        </p>
+      </div>
+    )}
+  </NavbarContext.Consumer>
 );
 
 export default NotFoundPage;
