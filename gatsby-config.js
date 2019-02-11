5d0b7c5e0ced67c441e9527e53428f5d97bb9237
Noalino <bgelineau.dev@gmail.com>
2019-02-11 12:45:32 +0100

    Add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,15 @@ module.exports = {
     ],
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.TRACKING_ID,
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
