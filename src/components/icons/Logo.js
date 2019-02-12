313669a585b5a0bbbe604d8476a4848517fa7652
Noalino <bgelineau.dev@gmail.com>
2019-02-12 15:39:12 +0100

    Fix logo sizes

--- src/components/icons/Logo.js
+++ src/components/icons/Logo.js
@@ -3,31 +3,17 @@ import React from 'react';
 import styles from '../../styles/icons.module.scss';
 
 const Logo = () => (
-    // original icon size: 1500x1500
-    <svg viewBox="0 0 1600 1600" className={styles.logo} xmlns="http://www.w3.org/2000/svg">
-        <g id={styles.shadow} transform="translate(100.0, 80.0)" fill="none" fillRule="evenodd">
-            {/* <path id="background" fill="#86D3C4" d="M0 0h1500v1500H0z"/> */}
-            <g strokeWidth="50">
-                <path d="M60 850.591l358.564-449.414 184.308 231.006"/>
-                <path d="M418.564 850.591L928.83 231.5l510.264 619.091"/>
-            </g>
-            <g strokeWidth="50">
-                <path d="M60 851.634l254.185 228.036m660.613 188.818l464.656-416.854"/>
-                <path d="M312.834 1078.323H696.68M710.472 1266.197h266.625" strokeLinecap="round"/>
-            </g>
-            <path d="M60 851h1379" strokeWidth="50" strokeLinecap="round"/>
+    // original size: width="1429" height="1100" (ratio 1.3)
+    <svg viewBox="0 0 1489 1160" className={styles.logo} xmlns="http://www.w3.org/2000/svg">
+        <g id={styles.shadow} transform="translate(60.0, 60.0)" strokeWidth="50" fill="none" fillRule="evenodd">
+            <path d="M25 658.591l358.564-449.414 184.308 231.006"/>
+            <path d="M383.564 658.591L893.83 39.5l510.264 619.091M25 659.634L279.185 887.67m660.613 188.818l464.656-416.854"/>
+            <path d="M277.834 886.323H661.68M675.472 1074.197h266.625M25 659h1379" strokeLinecap="round"/>
         </g>
-        <g id={styles.icon} fill="none" fillRule="evenodd">
-            {/* <path id="background" fill="#86D3C4" d="M0 0h1500v1500H0z"/> */}
-            <g strokeWidth="50">
-                <path d="M60 850.591l358.564-449.414 184.308 231.006"/>
-                <path d="M418.564 850.591L928.83 231.5l510.264 619.091"/>
-            </g>
-            <g strokeWidth="50">
-                <path d="M60 851.634l254.185 228.036m660.613 188.818l464.656-416.854"/>
-                <path d="M312.834 1078.323H696.68M710.472 1266.197h266.625" strokeLinecap="round"/>
-            </g>
-            <path d="M60 851h1379" strokeWidth="50" strokeLinecap="round"/>
+        <g id={styles.icon} strokeWidth="50" fill="none" fillRule="evenodd">
+            <path d="M25 658.591l358.564-449.414 184.308 231.006"/>
+            <path d="M383.564 658.591L893.83 39.5l510.264 619.091M25 659.634L279.185 887.67m660.613 188.818l464.656-416.854"/>
+            <path d="M277.834 886.323H661.68M675.472 1074.197h266.625M25 659h1379" strokeLinecap="round"/>
         </g>
     </svg>
 );
