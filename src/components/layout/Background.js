149e4d06aa7ed09993e5685cbc833b98e6b20997
Benoît Gelineau <bgelineau.dev@gmail.com>
2019-02-13 10:01:19 +0100

    Add particles background homepage

--- src/components/layout/Background.js
+++ src/components/layout/Background.js
@@ -1,34 +1,14 @@
 import React from 'react';
-import randomize from '../../utils/randomize';
+import Particles from 'react-particles-js';
 
+import params from '../../utils/particles-config';
 import styles from '../../styles/layout/layout.module.scss';
 
-const Background = () => {
-  // Create dots with random position & size
-  const dots = [];
-  for(let i = 0; i < 15; i++) {
-    const posX = randomize(2, 98, '%');
-    const posY = randomize(5, 95, '%');
-    const size = randomize(3, 10, 'px');
-    dots.push(
-      <div
-        key={i}
-        className={styles.dot}
-        style={{
-          top: posY,
-          left: posX,
-          width: size,
-          height: size
-        }}
-      />
-    );
-  }
-
-  return (
-    <div>
-      {dots}
-    </div>
-  );
-};
+const Background = () => (
+  <Particles
+    canvasClassName={styles.background}
+    params={params}
+  />
+);
 
 export default Background;
