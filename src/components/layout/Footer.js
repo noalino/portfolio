26b925c49d4239e6d071fa97ed82c8e6db8ddfde
Benoît Gelineau <bgelineau.dev@gmail.com>
2019-08-02 22:52:33 +0200

    Update city & footer date

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,12 +4,16 @@ import Triangle from '../icons/Triangle';
 
 import styles from '../../styles/layout/footer.module.scss';
 
-const Footer = () => (
-  <footer id={styles.footer}>
-    &copy; {new Date().getFullYear()} Benoît Gelineau
-    <Triangle className={styles.triangle} />
-    All rights reserved
-  </footer>
-);
+const Footer = () => {
+  const actualDate = new Date().getFullYear();
+  const date = actualDate === 2019 ? 2019 : `2019 - ${actualDate}`;
+  return (
+    <footer id={styles.footer}>
+      &copy; {date} Benoît Gelineau
+      <Triangle className={styles.triangle} />
+      All rights reserved
+    </footer>
+  )
+};
 
 export default Footer;
