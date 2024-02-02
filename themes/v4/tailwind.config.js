284b8a3caba8e43bf034cd57b6affd3ecf470e98
Noalino <bgelineau.dev@gmail.com>
2024-02-02 12:29:06 +0100

    Add custom fonts

--- themes/v4/tailwind.config.js
+++ themes/v4/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html"],
   theme: {
+    fontFamily: {
+      header: ["Bebas Neue", ...defaultTheme.fontFamily.sans],
+      sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
